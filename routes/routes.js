5712f32ddf8030e04d9116a2253ef8c96d3c84d9
spinxsunyi <just_eka@ymail.com>
2023-05-21 08:27:18 +0700

    updates the "update post patch", with timestamp

--- routes/routes.js
+++ routes/routes.js
@@ -13,7 +13,7 @@ router.get('/', async (req,res) => {
     }
 });
 
-router.post('/post', async (req,res) => {
+router.post('/postNewTask', async (req,res) => {
     let lastUpdated = new Date();
     const data = new Model({
         taskTitle: req.body.title,
@@ -26,6 +26,22 @@ router.post('/post', async (req,res) => {
         const dataToSave = await data.save();
         res.status(200).json(dataToSave);
         console.log(`data is saved: ${dataToSave}`)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
+})
+
+router.patch('/update/:id', async (req,res) => {
+    try{
+        const id = req.params.id;
+        req.body.lastUpdated = new Date();
+        const updatedData = req.body;
+        const options = {new:true};
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        );
+        res.send(result);
     }
     catch(error){
         res.status(400).json({message: error.message})
